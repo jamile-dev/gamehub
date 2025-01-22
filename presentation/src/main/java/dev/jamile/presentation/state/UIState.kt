1ef45f5a8bf7e83b681ca6d73f69dc2cfc8a890e
Jamile <jamile.bastos.dev@gmail.com>
2025-01-22 07:21:49 -0300

    Add UIState to handle success, error, loading on the app calls

---
+++ presentation/src/main/java/dev/jamile/presentation/state/UIState.kt
@@ -0,0 +1,7 @@
+package dev.jamile.presentation.state
+
+sealed class UIState<out T> {
+    data object Loading : UIState<Nothing>()
+    data class Success<out T>(val data: T) : UIState<T>()
+    data class Error(val message: String) : UIState<Nothing>()
+}
