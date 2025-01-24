e7cc305bd00fe46a63694f1a604de167383fec87
Jamile <jamile.bastos.dev@gmail.com>
2025-01-24 09:01:40 -0300

    Add Genre and GenreChip components

---
+++ presentation/src/main/java/dev/jamile/presentation/components/GenreChip.kt
@@ -0,0 +1,58 @@
+package dev.jamile.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+/**
+ * A composable that displays a list of genre chips.
+ *
+ * @param genres The list of genres to display.
+ * @param modifier Optional [Modifier] to apply to the [GenreChips] container.
+ */
+@Composable
+fun GenreChips(genres: List<String>?, modifier: Modifier = Modifier) {
+    Row(modifier = Modifier.padding(horizontal = 4.dp)) {
+        genres?.filter { it.isNotBlank() }?.take(2)?.forEach { genre ->
+            GenreChip(genre = genre)
+            Spacer(modifier = Modifier.width(4.dp))
+        }
+    }
+}
+
+/**
+ * A composable that displays a single genre chip.
+ *
+ * @param genre The genre to display.
+ */
+@Composable
+fun GenreChip(genre: String) {
+    val color = when (genre) {
+        "Action" -> Color(0xFFDF686C)
+        "Adventure" -> Color(0xFFFF9F1C)
+        "RPG" -> Color(0xFFC8D6AF)
+        "Shooter" -> Color(0xFFC8D6AF)
+        "Indie" -> Color(0xFFC8D6AF)
+        else -> Color(0xFFF5EE9E)
+    }
+    val backgroundColor = color.copy(alpha = 0.4f)
+    Box(
+        modifier = Modifier
+            .background(backgroundColor, RoundedCornerShape(4.dp))
+            .border(1.dp, color, RoundedCornerShape(4.dp))
+            .padding(horizontal = 12.dp, vertical = 6.dp)
+    ) {
+        Text(text = genre, color = Color.White, style = MaterialTheme.typography.bodySmall)
+    }
+}
