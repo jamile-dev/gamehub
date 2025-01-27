0e0f2ad19a5885874ee32fe1f277f3009a2c35b0
Jamile <jamile.bastos.dev@gmail.com>
2025-01-27 07:17:19 -0300

    refactor: rename file

--- data/src/main/java/dev/jamile/data/converters/GsonConverters.kt
+++ data/src/main/java/dev/jamile/data/converters/GsonConverters.kt
@@ -4,7 +4,7 @@ import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
-class Converters {
+class GsonConverters {
     @TypeConverter
     fun fromStringList(value: String?): List<String>? =
         value?.let {
--- data/src/main/java/dev/jamile/data/local/GameDatabase.kt
+++ data/src/main/java/dev/jamile/data/local/GameDatabase.kt
@@ -3,11 +3,11 @@ package dev.jamile.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import dev.jamile.data.converters.Converters
+import dev.jamile.data.converters.GsonConverters
 import dev.jamile.domain.models.GameDetails
 
 @Database(entities = [GameDetails::class], version = 1, exportSchema = false)
-@TypeConverters(Converters::class)
+@TypeConverters(GsonConverters::class)
 abstract class GameDatabase : RoomDatabase() {
     abstract fun favoriteGameDao(): FavoriteGameDao
 }
